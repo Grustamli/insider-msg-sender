69147d0cecada6f6e41973f7d18f0cabda27b97e
Gadir Rustamli <16738172+Grustamli@users.noreply.github.com>
2025-06-20 00:13:58 +0400

    feat: implemented missing methods ;

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -2,6 +2,8 @@ package daemon
 
 import (
 	"context"
+	"github.com/rs/zerolog"
+	"sync"
 	"time"
 )
 
@@ -13,9 +15,12 @@ type Daemon interface {
 }
 
 type TimerDaemon struct {
-	job    ScheduledJobFunc
-	period time.Duration
-	stop   chan struct{}
+	job     ScheduledJobFunc
+	period  time.Duration
+	stop    chan struct{}
+	logger  *zerolog.Logger
+	running bool
+	mu      sync.Mutex
 }
 
 var _ Daemon = (*TimerDaemon)(nil) // Ensure TimerDaemon implements Daemon
@@ -29,9 +34,54 @@ func NewTimerDaemon(job ScheduledJobFunc, period time.Duration) *TimerDaemon {
 }
 
 func (t *TimerDaemon) Start(ctx context.Context) error {
-	panic("implement me")
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if t.running {
+		return nil
+	}
+
+	t.running = true
+
+	go t.runJob(ctx)
+
+	return nil
 }
 
 func (t *TimerDaemon) Stop(ctx context.Context) error {
-	panic("implement me")
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if !t.running {
+		return nil // Already stopped
+	}
+	close(t.stop)                // Signal the daemon to stop
+	t.stop = make(chan struct{}) // Reset the channel for future starts
+	return nil
+}
+
+func (t *TimerDaemon) runJob(ctx context.Context) {
+	defer func() {
+		t.mu.Lock()
+		t.running = false
+		t.mu.Unlock()
+	}()
+
+	ticker := time.NewTicker(t.period)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.stop:
+			return
+		case <-ticker.C:
+			// Execute the job, but don't let it block the daemon
+			go func() {
+				if err := t.job(ctx); err != nil {
+					t.logger.Error().Err(err).Msg("job failed")
+				}
+			}()
+		}
+	}
 }
